44339b65844e50f00a8838fd74f4a40365b2091a
gba-exe <gabriel.baraujo@sptech.school>
2023-06-12 02:07:07 -0300

    feat(): Added database script.

--- server/site/src/database/gbaStudios.sql
+++ server/site/src/database/gbaStudios.sql
@@ -16,6 +16,12 @@ CREATE TABLE usuario (
 );
 
 insert into usuario values (null, 'gba.exe', 'gabriel.araujo1805@hotmail.com', '1206', 4, null);
+insert into usuario values (null, 'Clara', 'maria.clara@hotmail.com', '1206', 1, 1);
+insert into usuario values (null, 'gigi.exe', 'giovanna.rodrigues@hotmail.com', '12345678', 2, 1);
+insert into usuario values (null, 'Hideki', 'vitor.nakamura@hotmail.com', '12345678', 2, 1);
+insert into usuario values (null, 'Nz', 'nicolas.zanardi@hotmail.com', '12345678', 3, 1);
+insert into usuario values (null, 'Babi', 'barbara.barbato@hotmail.com', '12345678', 3, 1);
+insert into usuario values (null, 'Kauan', 'kauan.rodrigues@hotmail.com', '12345678', 1, 1);
 
 -- -----------------------------------------------------
 -- Table endereco
@@ -44,3 +50,12 @@ CREATE TABLE  login (
   PRIMARY KEY (fkUsuario, dataLogin),
   FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
 );
+
+insert into login values (1, '2023-05-18,09:30'),
+						 (2, '2023-05-06,09:30'),
+						 (2, '2023-05-07,11:30'),
+						 (3, '2023-06-08,20:30'),
+						 (4, '2023-06-08,20:30'),
+						 (5, '2023-06-01,20:30'),
+						 (6, '2023-06-10,20:30'),
+						 (7, '2023-06-10,20:30');
